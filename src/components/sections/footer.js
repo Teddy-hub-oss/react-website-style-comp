98168abe0ca0646fe7b25e7bab3111702de01990
teddy <teddyal3838@gmail.com>
2021-05-19 03:59:33 +0300

    add

--- src/components/sections/footer.js
+++ src/components/sections/footer.js
@@ -4,21 +4,8 @@ import { Link } from 'react-router-dom'
 import { AiFillLinkedin, AiFillGithub } from "react-icons/ai"
 const Footer = () => {
     return (
-        <footer>
-        <a
-        href="http://www.linkedin.com/in/teddyalemayehu"
-        target="_blank"
-        >
-        <img src="images/linkedin_logo.png" />
-        </a>
-
-        <a href="https://github.com/Teddy-hub-oss/react-pro.git"
-        target="_blank"
-        >
-        <img src="images/github_logo.png" 
-        alt="person picture"/>
-        </a>
-        </footer>
+    <>
+    </>
     
     )
 }
