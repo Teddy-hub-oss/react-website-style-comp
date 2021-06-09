fba38439bd241e73b224be812d41556bb0116d4d
teddy <teddyal3838@gmail.com>
2021-06-09 14:01:42 +0300

    added index files

---
+++ src/components/InfoSection/InfoSection.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {InfoSec} from './InfoSection.element'
+
+const InfoSection = ({ lightBg }) => {
+    return (
+        <>
+         <infoSec lightBg={lightBg}>h1</infoSec>   
+        </>
+    )
+}
+
+export default InfoSection
--- src/container/App.js
+++ src/container/App.js
@@ -2,9 +2,9 @@ import React from 'react'
 import './App.css'
 import { IconContext } from 'react-icons'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-import { Navbar } from '../components/sections'
+import { Navbar } from '../components/Navbar'
 import GlobalStyle from '../globalStyles'
-
+import Home from '../components/pages/HomePage/Home'
 
 const App = () => {
   return (
@@ -12,8 +12,10 @@ const App = () => {
    <Router>
    <GlobalStyle />
    <Navbar />
+   <Switch>
+      <Route path="/" exact component={Home} />
+   </Switch>
    </Router>
-
 
   )
 }
