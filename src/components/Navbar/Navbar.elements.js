798cd8f43692099700d323ae4dfa3fc6167f461c
teddy <teddyal3838@gmail.com>
2021-06-12 05:17:36 +0300

    add

--- src/components/Navbar/Navbar.elements.js
+++ src/components/Navbar/Navbar.elements.js
@@ -73,7 +73,7 @@ export const NavMenu = styled.ul `
 export const NavItem = styled.li `
   height: 80px;
   border-bottom: 2px solid transparent;
-font-size: 1.6rem;
+  font-size: 1.6rem;
 
   &:hover {
     border-bottom: 2px solid #4b59f7;
